e1dbf04f09d7630f3de634f5eddb454f2c3b695b
John Trupiano <jtrupiano@gmail.com>
2009-03-06 14:05:12 -0500

    Add auto-installation + activation for gems in preinitializer.rb

--- lib/conf/preinitializer.rb
+++ lib/conf/preinitializer.rb
@@ -8,13 +8,15 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 #perform_install = %w(development test).include?(rails_env)
 
 # Activate common gems
-common_gemfile = 'config/geminstaller.yml'
-#GemInstaller.install('--sudo --exceptions --config=config/geminstaller.yml') if perform_install
+common_gemfile = File.join(RAILS_ROOT, 'config', 'geminstaller.yml')
+GemInstaller.install('--sudo --exceptions --config=#{common_gemfile}') if perform_install
+Gem.refresh # in the event that we did install new gems, we'll need to update the cache so that we can activate them
 GemInstaller.autogem("--sudo --exceptions --config=#{common_gemfile}") if File.exists?(common_gemfile)
 
 # Activate environment-specific gems
 path = File.join(RAILS_ROOT, "config", rails_env, "geminstaller.yml")
 if File.exists?(path)
-  #GemInstaller.install("--sudo --exceptions --config=#{path}") if perform_install
+  GemInstaller.install("--sudo --exceptions --config=#{path}") if perform_install
+  Gem.refresh # in the event that we did install new gems, we'll need to update the cache so that we can activate them
   GemInstaller.autogem("--sudo --exceptions --config=#{path}")
 end
