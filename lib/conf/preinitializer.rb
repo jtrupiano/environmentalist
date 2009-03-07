a812faea2ad38bc6d450762d588613dee458c51c
John Trupiano <jtrupiano@gmail.com>
2009-03-06 21:12:38 -0500

    Prepare for GemInstaller -v0.5.2 (not released yet), which fixes the #install then #autogem problem.  Clean up preinitializer a bit

--- lib/conf/preinitializer.rb
+++ lib/conf/preinitializer.rb
@@ -1,22 +1,20 @@
 # Ensure that proper versions of gems in config/geminstaller.yml are activated.
 require 'rubygems'
 gem 'rails', '= 2.3.0'
-gem 'geminstaller', '= 0.5.1'
+gem 'geminstaller', '= 0.5.2'
 require 'geminstaller'
 
 rails_env = ENV['RAILS_ENV'] || 'development'
-#perform_install = %w(development test).include?(rails_env)
 
-# Activate common gems
+perform_install = %w(development test).include?(rails_env)
+
+# Activate gems
 common_gemfile = File.join(RAILS_ROOT, 'config', 'geminstaller.yml')
-GemInstaller.install('--sudo --exceptions --config=#{common_gemfile}') if perform_install
-Gem.refresh # in the event that we did install new gems, we'll need to update the cache so that we can activate them
-GemInstaller.autogem("--sudo --exceptions --config=#{common_gemfile}") if File.exists?(common_gemfile)
+env_gemfile    = File.join(RAILS_ROOT, "config", rails_env, "geminstaller.yml")
 
-# Activate environment-specific gems
-path = File.join(RAILS_ROOT, "config", rails_env, "geminstaller.yml")
-if File.exists?(path)
-  GemInstaller.install("--sudo --exceptions --config=#{path}") if perform_install
-  Gem.refresh # in the event that we did install new gems, we'll need to update the cache so that we can activate them
-  GemInstaller.autogem("--sudo --exceptions --config=#{path}")
+[common_gemfile, env_gemfile].each do |file|
+  if File.exists?(file)
+    GemInstaller.install("--exceptions --config=#{common_gemfile}") if perform_install
+    GemInstaller.autogem("--exceptions --config=#{common_gemfile}")
+  end
 end
