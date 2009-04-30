3d1fbeb1caf9e99fdc418e61c348d4e746a1e720
John Trupiano <jtrupiano@gmail.com>
2009-04-30 16:31:57 -0400

    Fix bug preventing nested geminstaller files to function appropriately.

--- lib/conf/preinitializer.rb
+++ lib/conf/preinitializer.rb
@@ -1,7 +1,7 @@
 # Ensure that proper versions of gems in config/geminstaller.yml are activated.
 require 'rubygems'
-gem 'rails', '= 2.3.0'
-gem 'geminstaller', '= 0.5.2'
+gem 'rails', '~> 2.3.2'
+gem 'geminstaller', '~> 0.5.1'
 require 'geminstaller'
 
 rails_env = ENV['RAILS_ENV'] || 'development'
@@ -14,7 +14,7 @@ env_gemfile    = File.join(RAILS_ROOT, "config", rails_env, "geminstaller.yml")
 
 [common_gemfile, env_gemfile].each do |file|
   if File.exists?(file)
-    GemInstaller.install("--exceptions --config=#{common_gemfile}") if perform_install
-    GemInstaller.autogem("--exceptions --config=#{common_gemfile}")
+    GemInstaller.install("--exceptions --config=#{file}") if perform_install
+    GemInstaller.autogem("--exceptions --config=#{file}")
   end
 end
